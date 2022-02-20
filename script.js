b930937e51d92befad245f18532f4278b17c5db3
najmul009 <najmul.tk16@gmail.com>
2022-02-20 21:29:52 +0600

    problem-3 done

--- script.js
+++ script.js
@@ -36,3 +36,34 @@ function pandaCost(singara,chomoca,jilapi){
 
 // const totalPandaCost = pandaCost(4,9,5)
 // console.log(totalPandaCost)
+
+//problem-3
+function picnicBudget(people){
+    let first100 = 5000;
+    let second100 = 4000;
+    let restpeople = 3000;
+
+    if(typeof(people) == 'string' || people <0){
+        return 'Input a valid Number'
+    }
+    else if(people <= 100){
+        first100Budget = first100 * people
+        return first100Budget;
+    }
+    else if (people <=200){
+        first100Budget = first100 * 100
+        second100Budget = second100 * (people -100) 
+        second100Budget +=  first100Budget;
+        return second100Budget 
+    }
+    else {
+        first100Budget = first100 * 100
+        second100Budget = second100 * 100
+        restPeopleBudget = restpeople * (people -200)
+        totalPeopleBudget = first100Budget + second100Budget + restPeopleBudget
+        return totalPeopleBudget;
+    }
+}
+
+// const totalBudget = picnicBudget(201)
+// console.log(totalBudget)
